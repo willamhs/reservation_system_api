2d54a2669a0ea0f6fed7d999e818367db745fb03
scar web <willamhuamansaavedra13@gmail.com>
2024-08-21 17:49:14 -0500

    first commit

---
+++ src/main/java/com/example/reservation_system_api/ReservationSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservation_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reservation_system_api/ReservationSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservation_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
